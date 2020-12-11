aa35e2d293ef3d31396066a16471df085ab0cc91
rubysattar <rubysattar@gmail.com>
2020-12-11 15:47:49 -0500

    updated background warm and cold to paisley

--- src/App.js
+++ src/App.js
@@ -37,11 +37,7 @@ function App() {
 
   return (
     <div className={
-      (typeof weather.main != "undefined") 
-      ? ((weather.main.temp > 16) 
-        ? 'app warm' 
-        : 'app') 
-      : 'app'}>
+      (typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className ="search-box">
           <input
